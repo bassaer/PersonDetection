c6cf2b7c263813d9cebe9056f5c2b71878eb611f
Nakayama <rf.1799@gmail.com>
2015-10-15 15:06:43 +0900

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Nakayama on 2015/10/15.
+//  Copyright (c) 2015年 Nakayama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
